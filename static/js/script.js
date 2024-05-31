67160bcf27dabb79e5c3835a8442a90cba44ae5b
Vlad <vlad.lavrynenko@powercoders.org>
2024-05-31 14:57:18 +0200

    Add a data set, json with Apple products.

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,20 @@
+const jsonFilePath = './static/js/data.json';
+
+fetch(jsonFilePath)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to fetch JSON');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching JSON:', error);
+  }); 
+
+
 let localTest = "";
 
 saveToLocalStorage();
@@ -7,10 +24,10 @@ getTitle();
 function saveToLocalStorage() {
     let title = document.querySelector("h1").textContent;
     localStorage.setItem("title", title);
-    alert("Title is saved.");
+    // alert("Title is saved.");
 }
 
 function getTitle() {
     let getLocalTitle = localStorage.getItem("title");
-    alert(getLocalTitle);
+    // alert(getLocalTitle);
 }
