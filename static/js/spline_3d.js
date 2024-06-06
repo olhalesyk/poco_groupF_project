e0c08c9519c5991f260936b21299d153bbad8c85
olhalesyk <olha.lesyk@powercoders.org>
2024-06-06 22:29:22 +0200

    Change styles

--- static/js/spline_3d.js
+++ static/js/spline_3d.js
@@ -4,33 +4,3 @@ const canvas = document.getElementById("canvas3d");
 const app = new Application(canvas);
 app.load("https://prod.spline.design/6E-m9MtHDNrpdA9L/scene.splinecode");
 // 3D Spline End
-
-(function () {
-  function iv(a) {
-    if (typeof a.getBoundingClientRect !== "function") {
-      return false;
-    }
-    var b = a.getBoundingClientRect();
-    return (
-      b.bottom + 50 >= 0 &&
-      b.right + 50 >= 0 &&
-      b.top - 50 <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      b.left - 50 <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-  function ll() {
-    var elements = document.querySelectorAll(
-      ".animation-left:not(.lineUp-animation)",
-    );
-    elements.forEach(function (element) {
-      if (iv(element)) {
-        element.classList.add("lineUp-animation");
-      }
-    });
-    if (elements.length > 0) {
-      requestAnimationFrame(ll);
-    }
-  }
-  requestAnimationFrame(ll);
-})();
