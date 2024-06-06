14f9092f7f68c3921dfb9f800174aabc9f9762a7
olhalesyk <olha.lesyk@powercoders.org>
2024-06-06 17:52:52 +0200

    Add 3D section and MacBook section according to changed design

--- static/js/spline_3d.js
+++ static/js/spline_3d.js
@@ -2,5 +2,5 @@
 import { Application } from "https://cdn.skypack.dev/@splinetool/runtime";
 const canvas = document.getElementById("canvas3d");
 const app = new Application(canvas);
-app.load("https://prod.spline.design/5Xf59aVv6uQ6WCaV/scene.splinecode");
+app.load("https://prod.spline.design/6E-m9MtHDNrpdA9L/scene.splinecode");
 // 3D Spline End
